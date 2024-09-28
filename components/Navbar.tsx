7f43a3fc590c73bf06f4b0a2d61bf8b93b707bc1
jorelm68 <methan@umich.edu>
2024-09-28 13:11:32 -0400

    Outliend the components and installed bootstrap

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
 // Log in with spotify button (if not authenticated)
 // Log out button (if authenticated)
 
+// Generative UI factors:
+// Backdrop color
+// Button color
+
 export default function Navbar() {
     return (
         <nav>
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,6 +1,11 @@
 // Screen for when you are actively in a game
 // this is the bulk of the project
 
+// List of categories: Danceability, Energy, Key, Loudness, Mode, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo, Time Signature, Popularity
+// Danceability, Energy, Loudness, Speechiness, Acousticness, Instrumentalness, Liveness, Valence
+
+// Danceability, Valence (happiness), Loudness, Acousticness
+
 export default function Game() {
     return (
         <>
