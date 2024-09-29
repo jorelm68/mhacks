75a7b587bbf9c937d622beb568df38e2cbf70ffd
jorelm68 <methan@umich.edu>
2024-09-28 20:44:56 -0400

    Added more info for waiting for game

--- components/WaitingForGame.tsx
+++ components/WaitingForGame.tsx
@@ -5,11 +5,14 @@ interface WaitingForGameProps {
 }
 
 export default function WaitingForGame({ game }: WaitingForGameProps) {
+
+
     return (
         <div>
             <h1>Waiting for game</h1>
             {game.name && <h2>{game.name}</h2>}
-            <p>This is the waiting for game component</p>
+            <p>Player 1: {game.profile1 ? game.profile1 : 'Searching...'}</p>
+            <p>Player 2: {game.profile2 ? game.profile2 : 'Searching...'}</p>
         </div>
     )
 }
